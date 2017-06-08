cff513cf8482e03a37bb09c3e696eec580b27200
Monique Rio <nique.rio@gmail.com>
2017-06-07 22:14:10 -0400

    added database.yml back into repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,6 @@ namespace :deploy do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env)  do
-          execute :bundle
           execute :rake, 'fake_data:all'
         end
       end
