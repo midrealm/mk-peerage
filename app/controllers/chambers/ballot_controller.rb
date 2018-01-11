6faacb5476015f9d9639f5cbfdc6a7852f99c609
Monique Rio <nique.rio@gmail.com>
2018-01-11 07:59:52 -0500

    peer/poll/candidates_controller inherits from chambers/ballot_controller

---
+++ app/controllers/chambers/ballot_controller.rb
@@ -0,0 +1,44 @@
+class Chambers::BallotController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_peer
+  helper_method :peerage
+
+  def index
+    if Poll.current(peerage).nil?
+      redirect_to chambers_path
+    else
+      @ballot = Ballot.new(current_user.peer(peerage)) 
+    end
+  end
+  def update
+    @advising = BallotEntry.new(candidate: Candidate.find(params[:id]), peer: current_user.peer(peerage)).advising
+    if @advising.update(advising_params)
+      @advising.update(submitted: true)  
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+  
+  def edit
+    @candidate = Candidate.find(params[:id]) 
+    @advising = BallotEntry.new(candidate: @candidate, peer: current_user.peer(peerage)).advising
+  end
+  
+  
+  private
+  def advising_params
+    params.require(:advising).permit(:judgement, :comment)
+  end
+  def authorize_peer
+    authorize! :take_poll, peerage
+  end
+
+  def peerage
+  end
+
+  def self.controller_path
+    'chambers/peerage/poll/candidates'
+  end
+end
+
--- app/controllers/chambers/laurel/poll/candidates_controller.rb
+++ app/controllers/chambers/laurel/poll/candidates_controller.rb
@@ -1,46 +1,6 @@
-class Chambers::Laurel::Poll::CandidatesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authorize_laurel
-  helper_method :peerage
-
-  def index
-    if Poll.current(:laurel).nil?
-      redirect_to chambers_path
-    else
-      @ballot = Ballot.new(current_user.peer(peerage)) 
-    end
-  end
-  def update
-    @advising = BallotEntry.new(candidate: Candidate.find(params[:id]), peer: current_user.laurel).advising
-    if @advising.update(advising_params)
-      @advising.update(submitted: true)  
-      redirect_to action: :index
-    else
-      render :edit
-    end
-  end
-  
-  def edit
-    @candidate = Candidate.find(params[:id]) 
-    @advising = BallotEntry.new(candidate: @candidate, peer: current_user.laurel).advising
-  end
-  
-  
-  private
-  def advising_params
-    params.require(:advising).permit(:judgement, :comment)
-  end
-  def authorize_laurel
-    authorize! :take_poll, :laurel
-  end
-
-
+class Chambers::Laurel::Poll::CandidatesController < Chambers::BallotController
   def peerage
     :laurel
-  end
-
-  def self.controller_path
-    'chambers/peerage/poll/candidates'
   end
 end
 
