3026bd935fd771ae29c45ed8fc2e3f1c0c89568c
Monique Rio <nique.rio@gmail.com>
2018-01-10 23:08:13 -0500

    Removed Judgement from db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  Judgement.create([{name: 'Elevate'}, {name: 'Drop'}, {name: 'No Strong Opinion'}, {name: 'Wait'}])
 
