ba13023bb39f604ad26d09a5fb9c0bbf4f897e9a
Monique Rio <nique.rio@gmail.com>
2018-01-15 18:23:28 -0500

    removed duplication from routes; moved royalty controller to be under chambers namespace

--- config/routes.rb
+++ config/routes.rb
@@ -12,51 +12,63 @@ Rails.application.routes.draw do
     resources :images, only: [:create]
     resources :comments, only: [:create]
 
-    get '/:peerage/groups/:slug' => 'groups#show', as: :group
-    get '/:peerage/groups' => 'groups#index', as: :groups
+    namespace :admin do
+      resources :royalty, except: [:destroy, :show] 
+    end
 
-    get '/:peerage/candidates/:id' => 'candidates#show', as: :candidate
-    get '/:peerage/candidates' => 'candidates#index', as: :candidates
-    get '/:peerage/candidates/:id/poll_comments' => 'candidates#poll_comments', as: :poll_comments
+    scope '/:peerage/' do
+      get '/groups/:slug' => 'groups#show', as: :group
+      get '/groups' => 'groups#index', as: :groups
 
-    get '/:peerage/poll' => 'ballot#index', as: :ballot
-    get '/:peerage/poll/candidates/:id' => 'ballot#edit', as: :edit_ballot_candidate
-    put '/:peerage/poll/candidates/:id' => 'ballot#update'
+      get '/candidates/:id' => 'candidates#show', as: :candidate
+      get '/candidates' => 'candidates#index', as: :candidates
+      get '/candidates/:id/poll_comments' => 'candidates#poll_comments', as: :poll_comments
 
-    get '/:peerage/admin/candidates' => 'admin/candidates#index', as: :admin_candidates
-    get '/:peerage/admin/candidates/new' => 'admin/candidates#new', as: :admin_new_candidate
-    post '/:peerage/admin/candidates' => 'admin/candidates#create', as: :admin_create_candidate
-    get '/:peerage/admin/candidates/:id/edit' => 'admin/candidates#edit', as: :admin_edit_candidate
-    patch '/:peerage/admin/candidates/:id' => 'admin/candidates#update', as: :admin_update_candidate
-    put '/:peerage/admin/candidates/:id' => 'admin/candidates#update'
-    delete '/:peerage/admin/candidates/:id' => 'admin/candidates#destroy', as: :admin_delete_candidate
+      
+      get '/poll' => 'ballot#index', as: :ballot
+      get '/poll/candidates/:id' => 'ballot#edit', as: :edit_ballot_candidate
+      put '/poll/candidates/:id' => 'ballot#update'
 
-    get '/:peerage/admin/poll/new' => 'admin/polls#new', as: :admin_new_poll
-    post '/:peerage/admin/poll' => 'admin/polls#create', as: :admin_create_poll
-    get '/:peerage/admin/poll/edit' => 'admin/polls#edit', as: :admin_edit_poll
-    patch '/:peerage/admin/poll' => 'admin/polls#update', as: :admin_update_poll
-    put '/:peerage/admin/poll' => 'admin/polls#update'
+      namespace :admin do
 
-    get '/:peerage/admin/peers' => 'admin/peers#index', as: :admin_peers
-    get '/:peerage/admin/peers/new' => 'admin/peers#new', as: :admin_new_peer
-    post '/:peerage/admin/peers' => 'admin/peers#create', as: :admin_create_peer
-    get '/:peerage/admin/peers/:id/edit' => 'admin/peers#edit', as: :admin_edit_peer
-    patch '/:peerage/admin/peers/:id' => 'admin/peers#update', as: :admin_update_peer
-    put '/:peerage/admin/peers/:id' => 'admin/peers#update'
+        get '/candidates' => 'candidates#index', as: :candidates
+        get '/candidates/new' => 'candidates#new', as: :new_candidate
+        post '/candidates' => 'candidates#create', as: :create_candidate
+        get '/candidates/:id/edit' => 'candidates#edit', as: :edit_candidate
+        patch '/candidates/:id' => 'candidates#update', as: :update_candidate
+        put '/candidates/:id' => 'candidates#update'
+        delete '/candidates/:id' => 'candidates#destroy', as: :delete_candidate
+
+        get '/poll/new' => 'polls#new', as: :new_poll
+        post '/poll' => 'polls#create', as: :create_poll
+        get '/poll/edit' => 'polls#edit', as: :edit_poll
+        patch '/poll' => 'polls#update', as: :update_poll
+        put '/poll' => 'polls#update'
+
+        get '/peers' => 'peers#index', as: :peers
+        get '/peers/new' => 'peers#new', as: :new_peer
+        post '/peers' => 'peers#create', as: :create_peer
+        get '/peers/:id/edit' => 'peers#edit', as: :edit_peer
+        patch '/peers/:id' => 'peers#update', as: :update_peer
+        put '/peers/:id' => 'peers#update'
+      end
+    end
 
   end
   scope :chambers do
     namespace :admin do
-      resources :royalty, except: [:destroy, :show] 
+#      resources :royalty, except: [:destroy, :show] 
     end 
   end
 
-  get '/:peerage/groups' => 'groups#index', as: :groups
-  get '/:peerage/groups/:slug' => 'groups#show', as: :group
-  get '/:peerage/roll_of_honor' => 'peers#index', as: :roll_of_honor
-  get '/:peerage/:slug' => 'peers#show', as: :peer
-  get '/:peerage/:slug/contact' => 'contact#new', as: :contact
-  post '/:peerage/:slug/contact' => 'contact#create'
+  scope '/:peerage/' do
+    get '/groups' => 'groups#index', as: :groups
+    get '/groups/:slug' => 'groups#show', as: :group
+    get '/roll_of_honor' => 'peers#index', as: :roll_of_honor
+    get '/:slug' => 'peers#show', as: :peer
+    get '/:slug/contact' => 'contact#new', as: :contact
+    post '/:slug/contact' => 'contact#create'
+  end
 
   root to: "home#index" 
 
