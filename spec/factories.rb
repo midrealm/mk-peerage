e114a30f8721714a659524d6c61148fc7ca635a1
Monique Rio <nique.rio@gmail.com>
2018-02-02 13:52:54 -0500

    refactored factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,15 +5,6 @@ FactoryGirl.define do
     candidate
     document { File.new("#{Rails.root}/spec/fixtures/images/portrait.jpg") }
   end
-  factory :peer, aliases: [:laurel_peer]  do
-    active true
-    vigilant false
-    type "Laurel"
-    association :user
-  end
-  factory :pelican_peer, parent: :peer  do
-    type 'Pelican'
-  end
   factory :poll_result do
     candidate
     poll 
@@ -95,17 +86,15 @@ FactoryGirl.define do
     "person#{n}@example.com"
   end
 
-  factory :laurel_user, parent: :user do
-    sca_name 'Lester Laurel'
-    after(:create) do |u|
-      create(:peer, user: u, active: true, vigilant: false, type: 'Laurel')
-    end
+  factory :peer, aliases: [:laurel_peer]  do
+    active true
+    vigilant false
+    type "Laurel"
+    association :user
   end
-  factory :pelican_user, parent: :user do
-      sca_name 'Peter Pelican'
-      after(:create) do |u|
-        create(:peer, user: u, active: true, vigilant: false, type: 'Pelican')
-      end
+
+  factory :pelican_peer, parent: :peer  do
+    type 'Pelican'
   end
 
   factory :user do
@@ -115,41 +104,36 @@ FactoryGirl.define do
     sca_name 'Mundungus Jones'
   end
  
-  factory :pelican, class: User do
-    email {generate :email}
-    password Devise.friendly_token.first(8)  
-    sca_name "Penny Pelican"
-    group
-    after(:create) do |u|
-      create(:peer, user: u, active: true, vigilant: false, type: 'Pelican')
-    end
-  end
-
-  factory :admin, aliases: [:laurel_admin], class: User do
-    email {generate :email}
-    password Devise.friendly_token.first(8)  
-    sca_name "Mundugus Admin"
-    group
-    after(:create) do |u|
-       create(:peer, user: u, admin: true, active: true, vigilant: false, type: 'Laurel')
-    end
-     
-  end
-
-  factory :pelican_admin, class: User do
-    email {generate :email}
-    password Devise.friendly_token.first(8)  
-    sca_name "Mundingus Admin"
-    group
-    after(:create) do |u|
-       create(:peer, user: u, admin: true, active: true, vigilant: false, type: 'Pelican')
-    end
-  end
-
-  factory :royal, class: User do
-    email {generate :email}
-    password Devise.friendly_token.first(8)  
-    sca_name "Mundugus Jones"
+  factory :royal, parent: :user do
     royalty true
   end
+
+  factory :laurel_user, parent: :user do
+    sca_name 'Lester Laurel'
+    after(:create) do |u|
+      create(:laurel_peer, user: u)
+    end
+  end
+
+  factory :pelican_user, parent: :user do
+      sca_name 'Peter Pelican'
+      after(:create) do |u|
+        create(:pelican_peer, user: u)
+      end
+  end
+
+  factory :admin, aliases: [:laurel_admin], parent: :user do
+    sca_name "Mundugus Admin"
+    after(:create) do |u|
+       create(:laurel_peer, user: u, admin: true)
+    end
+  end
+
+  factory :pelican_admin, parent: :user do
+    sca_name "Mundingus Admin"
+    after(:create) do |u|
+       create(:pelican_peer, user: u, admin: true)
+    end
+  end
+
 end
