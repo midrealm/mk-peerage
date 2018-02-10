f69eac4123a1f0137fa81fc58285dc62c03fa7ff
Monique Rio <nique.rio@gmail.com>
2018-02-10 10:55:35 -0500

    added spec for flash

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe "layouts/application.html.erb" do
+  it "renders flash success and alert" do
+    flash[:success]  = "You were successful"
+    flash[:alert]  = "Actually you failed"
+    render template: 'layouts/application.html.erb', locals: { jskit: ''}
+    expect(rendered).to have_content("You were successful")
+    expect(rendered).to have_content("Actually you failed")
+  end
+end
