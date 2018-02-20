70acf734fc442cf28c7b32c5033bd237ee51b686
Monique Rio <nique.rio@gmail.com>
2018-02-20 09:30:53 -0500

    fontawesome works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
 import 'bootstrap'
 import './modules/datatable'
 import './modules/markdownTextField'
+
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+fontawesome.library.add(solid)
+fontawesome.library.add(regular)
