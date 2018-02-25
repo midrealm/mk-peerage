3b5ca040eac747cd8f3e04841577abe5f1b34585
Monique Rio <nique.rio@gmail.com>
2018-02-25 14:40:15 -0500

    got rid of hardcoding peerages in toggleImages module

--- app/assets/javascripts/modules/editUserToggle.js
+++ app/assets/javascripts/modules/editUserToggle.js
@@ -1,26 +1,23 @@
+import peerages from '../entities/peerages';
 export default (() => {
   function toggleImages(e){
     $('#croppie').addClass('d-none')
     $('input[data-behavior="profile-data"]').removeClass('active')
-    switch(e.target.hash){
-      case '#personal':
-          var input = $('#arms_cache')
-          var img = $('<img>', {src: input.val(), width: '100px'})
-          $('#profile_preview').html(img) 
-        break
-      case '#laurel':
-        peerImagePreview('#laurel')
-        break
-      case '#pelican':
-        peerImagePreview('#pelican')
-        break
-      default:
-    }
+
+    const tab = e.target.hash.substr(1);
+    if(tab === 'personal'){
+      var input = $('#arms_cache')
+      var img = $('<img>', {src: input.val(), width: '100px'})
+      $('#profile_preview').html(img) 
+    }else if(peerages.includes(tab)){
+      peerImagePreview(tab)
+    } 
+
     $('#profile_preview').removeClass('d-none')
       
   }; 
   function peerImagePreview(peer){
-    var input = $(peer + ' input[data-behavior="profile-data"]')
+    var input = $(`#${peer} input[data-behavior="profile-data"]`)
     input.addClass('active')
     var img = $('<img>').attr('src', input.val())
     $('#profile_preview').html(img) 
