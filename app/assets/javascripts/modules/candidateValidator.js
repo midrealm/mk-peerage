a83e7aa921c4f641e5bf8ba346a610f978c91d20
Monique Rio <nique.rio@gmail.com>
2019-06-21 22:00:12 -0400

    hotfix removal of javascript validators for candidate creation

--- app/assets/javascripts/modules/candidateValidator.js
+++ app/assets/javascripts/modules/candidateValidator.js
@@ -1,8 +1,8 @@
 export default (() => {
   function setup_error_divs(){
       create_error_div('#candidate_sca_name', 'Need SCA Name')
-      create_error_div('#candidate_group_id', 'Need Group')
-      create_error_div('#profile_pic_input', 'Need Profile Picture')
+      //create_error_div('#candidate_group_id', 'Need Group')
+      //create_error_div('#profile_pic_input', 'Need Profile Picture')
   }
   function create_error_div(id, message){
     var msg = $('<div>').addClass('invalid-feedback').text(message)
@@ -14,12 +14,12 @@ export default (() => {
     if($('#candidate_sca_name').val() == ''){
       $('#candidate_sca_name').addClass('is-invalid')
     } 
-    if($('#candidate_group_id').val() == ''){
-      $('#candidate_group_id').addClass('is-invalid')
-    }
-    if($.trim($('#profile_preview').html()).length === 0){
-      $('#profile_pic_input').addClass('is-invalid')
-    }
+    //if($('#candidate_group_id').val() == ''){
+    //  $('#candidate_group_id').addClass('is-invalid')
+    //}
+    //if($.trim($('#profile_preview').html()).length === 0){
+    //  $('#profile_pic_input').addClass('is-invalid')
+    //}
     if($('.is-invalid').length > 0){
       e.preventDefault(); 
     }
