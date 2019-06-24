1c91f547191d9a020f1cbecb0846409e2499d357
Monique Rio <nique.rio@gmail.com>
2019-06-24 15:20:07 -0400

    took out port 80 of production enviornment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "laurel_#{Rails.env}"
-  config.action_mailer.default_url_options = { host: 'mk-peerage.herokuapp.com', port: 80, protocol: 'https'}
+  config.action_mailer.default_url_options = { host: 'mk-peerage.herokuapp.com', protocol: 'https'}
   config.action_mailer.perform_caching = false
   ActionMailer::Base.smtp_settings = {
     :address          => "smtp.sendgrid.net",
