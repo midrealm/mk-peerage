89d9b9955d2f15f7c3448247b9d4acac6016ef0e
Monique Rio <nique.rio@gmail.com>
2020-03-03 14:00:13 -0500

    fixed uploading of pdfs and docx

--- spec/requests/chambers/documents_spec.rb
+++ spec/requests/chambers/documents_spec.rb
@@ -10,4 +10,24 @@ describe "post /chambers/documents" do
     expect(Document.count).to eq(1)
     expect(response).to redirect_to("/chambers/laurel/candidates/#{cand.id}#documents")
   end
+  it "creates new pdf for candidate" do
+    document = fixture_file_upload("#{fixture_path}/test.pdf", 'application/pdf')
+    laurel = create(:laurel_peer)
+    sign_in(laurel.user)
+    cand = create(:candidate)
+    expect(Document.count).to eq(0)
+    post '/chambers/documents', params: { :document => {candidate_id: cand.id, document: document, name: 'Name', description: 'Description'} }
+    expect(Document.count).to eq(1)
+    expect(response).to redirect_to("/chambers/laurel/candidates/#{cand.id}#documents")
+  end
+  it "creates new docx for candidate" do
+    document = fixture_file_upload("#{fixture_path}/test.docx", 'application/application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    laurel = create(:laurel_peer)
+    sign_in(laurel.user)
+    cand = create(:candidate)
+    expect(Document.count).to eq(0)
+    post '/chambers/documents', params: { :document => {candidate_id: cand.id, document: document, name: 'Name', description: 'Description'} }
+    expect(Document.count).to eq(1)
+    expect(response).to redirect_to("/chambers/laurel/candidates/#{cand.id}#documents")
+  end
 end
