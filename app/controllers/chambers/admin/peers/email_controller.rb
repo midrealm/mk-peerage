a5d9400e6004ecbda32fc2b46aeb310291e0da6d
Monique Rio <mrio@umich.edu>
2021-08-07 14:35:12 -0400

    updated methods for changing peer emails and sending resets

--- app/controllers/chambers/admin/peers/email_controller.rb
+++ app/controllers/chambers/admin/peers/email_controller.rb
@@ -2,15 +2,16 @@ class Chambers::Admin::Peers::EmailController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_admin
   def new
-    @peers = Peer.where_order(peerage).map{|x| ["#{x.sca_name} (#{x.email})", x.user.id] }
+    @user = User.find(params[:user_id])
   end
   def create
     @peers = Peer.where_order(peerage).map{|x| ["#{x.sca_name} (#{x.email})", x.user.id] }
     @user = User.find(params[:id])
     if @user.update(email: params[:email])
-       if @user.send_reset_password_instructions
-          flash[:success] = 'Email Changed and Reset Password Instructions Sent'
-          redirect_to chambers_path 
+       token = @user.send_reset_password_instructions
+       if token
+         flash[:success] = "<b>Email Changed for #{@user.sca_name}</b><br> Change Password Link:<br> #{edit_password_url(@user, reset_password_token: token)}"
+          redirect_to chambers_admin_peers_path(peerage)
         else
           flash[:error] = 'Email changed, but reset password instructions failed to send'
           render 'new' 
--- app/controllers/chambers/admin/peers/password_controller.rb
+++ app/controllers/chambers/admin/peers/password_controller.rb
@@ -6,13 +6,13 @@ class Chambers::Admin::Peers::PasswordController < ApplicationController
   end
   def create
     @user = User.find(params[:id])
-    if @user.send_reset_password_instructions
-      flash[:success] = 'Reset Password Instructions Sent'
-      redirect_to chambers_path 
+    token = @user.send_reset_password_instructions
+    if token
+      flash[:success] = "<b>Reset Password Instructions Sent to #{@user.sca_name}</b> <br> Change Password Link:<br> #{edit_password_url(@user, reset_password_token: token)}"
     else
       flash[:error] = "Couldn't Send Reset Password Instructions"
-      render 'new' 
     end
+    redirect_to chambers_admin_peers_path(peerage)
   end
 private
   def self.controller_path
