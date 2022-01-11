11a48d730e6c927fc56e5052223243e6b66108e0
Monique Rio <mrio@umich.edu>
2022-01-10 22:08:18 -0500

    changed Judgement to Counsel in views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   #For Devise
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3333 }
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
